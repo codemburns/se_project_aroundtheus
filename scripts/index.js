e2f6bfc524f06661ed1cbc1677fd3be3a3065d05
codemburns <mnburns95@gmail.com>
2025-07-09 00:19:19 -0600

    Add files via upload
    stage 3 upload

--- scripts/index.js
+++ scripts/index.js
@@ -26,3 +26,16 @@ const initialCards = [
 ];
 
 console.log(initialCards);
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal_opened");
+});
+
+const profileCloseButton = document.querySelector("#profile-close-modal");
+
+profileCloseButton.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal_opened");
+});
